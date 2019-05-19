403ea3875b18e45e04c91a73da9b4a7678552041
Taras Koshkin <tkoshkin@gmail.com>
2019-05-19 16:04:07 -0700

    Revert "--amend"
    This reverts commit 4fe806bbb4fc42b2c0ced33c618bb3eb14ae47d7.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
 import 'package:events/resources/AppColors.dart';
 import 'package:events/resources/AppTextStyles.dart';
 import 'package:events/ui/events/EventsList.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _appSecret;
+
+  _MyAppState() {
+    final ios = defaultTargetPlatform == TargetPlatform.iOS;
+    _appSecret = ios
+        ? "dc3aee2c-5d61-4850-914a-4fc16e743e0"
+        : "997e3c51-dda5-4cb9-b4ce-2d07a003749a";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  initPlatformState() async {
+    await AppCenter.start(_appSecret, [AppCenterCrashes.id]);
+  }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme.apply(
